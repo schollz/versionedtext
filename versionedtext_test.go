5dabb57845c71fedfc4055d4bb766a5a9f653e12
Zack Scholl <zack.scholl@gmail.com>
2017-03-21 16:46:01 -0600

    Updated test for integrated struct

--- versionedtext_test.go
+++ versionedtext_test.go
@@ -2,6 +2,7 @@ package versionedtext
 
 import (
 	"encoding/json"
+	"fmt"
 	"testing"
 )
 
@@ -9,20 +10,8 @@ func TestGeneral(t *testing.T) {
 	d := NewVersionedText("A word")
 	d.Update("A word and adding something at the end")
 	d.Update("A (deleted) and adding something at the end, with another addition")
-	bJSON, err := json.Marshal(d)
-	if err != nil {
-		t.Error(err)
-	}
 
-	var d2 VersionedText
-	err = json.Unmarshal(bJSON, &d2)
-	if err != nil {
-		t.Error(err)
-	}
-	if d2.GetCurrent() != d.GetCurrent() {
-		t.Errorf("Problem reloading from JSON")
-	}
-
+	// Test getting a snapshot
 	snapshots := d.GetSnapshots()
 	previousText, err := d.GetPreviousByTimestamp(snapshots[1])
 	if err != nil {
@@ -31,4 +20,27 @@ func TestGeneral(t *testing.T) {
 	if previousText != "A word and adding something at the end" {
 		t.Errorf("Did not reconstruct properly")
 	}
+
+	// Test putting them in a struct and marshaling
+	type SomeStruct struct {
+		SomeThing string
+		SomeDiffs VersionedText
+	}
+	var d1 SomeStruct
+	d1.SomeDiffs = d
+	d1.SomeThing = "Some thing"
+	bJSON, err := json.Marshal(d1)
+	fmt.Println(string(bJSON))
+	if err != nil {
+		t.Error(err)
+	}
+	var d2 SomeStruct
+	err = json.Unmarshal(bJSON, &d2)
+	if err != nil {
+		t.Error(err)
+	}
+	if d2.SomeDiffs.GetCurrent() != d.GetCurrent() {
+		t.Errorf("Problem reloading from JSON")
+	}
+
 }
